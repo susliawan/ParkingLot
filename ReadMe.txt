40cfe2cdcf0252f844b44627de09239ecb3a6f4e
Nur Adi Susliawan <adi.susliawan@gmail.com>
2016-12-25 14:21:32 +1000

    Finalize main class, Add ReadMe.txt and Export ParkingLot.jar

---
+++ ReadMe.txt
@@ -0,0 +1,8 @@
+The parking lot program is created using programming language Java with ParkingLot.jar as executable file.
+How to run the program:
+1. Open terminal and go to folder that contains ParkingLot.jar
+2. To run the program based on input command file, 
+	type "java -jar ParkingLot.jar [file_input_path] [file_output_path]"
+3. To run the program in interactive way based on user input,
+	type "java -jar ParkingLot.jar"
+4. To exit the program type "exit" in input command 
--- src/main/Main.java
+++ src/main/Main.java
@@ -7,28 +7,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		CommandParser parser = new CommandParser();
-		String inFilePath = "/Users/Susliawan/Desktop/input.txt";
-		String outFilePath = "/Users/Susliawan/Desktop/output.txt";
-		try {
-			String output = parser.commandByFile(inFilePath);
-			parser.saveOutputToFile(outFilePath, output);
-			System.out.println(output);
-		} catch (IOException e) {
-			System.out.println("File reading error");
-		}
-		
-		Scanner sc = new Scanner(System.in);
-		boolean exit = false;
-		while(!exit){
-			System.out.println("Input:");
-			String commandStr = sc.nextLine();
-			String[] commands = commandStr.split("\\s+");
-			String result = parser.command(commands);
-			if(result != null)
-				System.out.println("Output:\n" + result);
-			else
-				exit = true;	
-		}
-		sc.close();
+		if(args.length == 2){
+			// Command by file input and output
+			String inFilePath = args[0];
+			String outFilePath = args[1];
+			try {
+				String output = parser.commandByFile(inFilePath);
+				parser.saveOutputToFile(outFilePath, output);
+				System.out.println(output);
+				System.out.println("Sucessfully saved the outputs to " + outFilePath);
+			} catch (IOException e) {
+				System.out.println("File processing error");
+			}
+		}else if(args.length == 0){
+			// Command in interactive way based on user input
+			Scanner sc = new Scanner(System.in);
+			boolean exit = false;
+			while(!exit){
+				System.out.println("Input:");
+				String commandStr = sc.nextLine();
+				String[] commands = commandStr.split("\\s+");
+				String result = parser.command(commands);
+				if(result != null)
+					System.out.println("Output:\n" + result);
+				else
+					exit = true;	
+			}
+			sc.close();
+		}else
+			System.out.println("Parameters are incomplete");
 	}
 }
